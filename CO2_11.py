3e84fe4f920c9ac74a36c302016a667d5cbd43b2
Lijo Joseph <134631230+Lijojosephh@users.noreply.github.com>
2026-09-14 21:46:48 +0530

    Add files via upload

---
+++ CO2_11.py
@@ -0,0 +1,7 @@
+square = lambda a: a * a
+rectangle = lambda l, b: l * b
+triangle = lambda b, h: 0.5 * b * h
+
+print("Area of Square =", square(5))
+print("Area of Rectangle =", rectangle(5, 4))
+print("Area of Triangle =", triangle(6, 3))
